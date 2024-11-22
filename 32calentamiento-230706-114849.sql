334663612b81e11d67cfe8f184f2bb3c2b42541a
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-11-21 22:29:12 -0300

    Tarea para entrar en calor y operador BETWEEN

---
+++ 32calentamiento-230706-114849.sql
@@ -0,0 +1,10 @@
+-- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XX
+
+-- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XXX
+SELECT first_name, last_name, last_connection from users WHERE last_connection LIKE'221%';
+-- Nombre, apellido y seguidores(followers) de todos a los que lo siguen más de 4600 personas
+SELECT first_name, last_name, followers FROM users WHERE followers > 4600;
+
+-- Operador BETWEEN
+SELECT first_name, last_name, followers FROM users WHERE followers BETWEEN 4600 AND 4700
+order by followers desc;
