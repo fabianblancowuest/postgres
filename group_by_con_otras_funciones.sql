68254b83e463f80e73d128561c70a5a3d659a7a2
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-05-08 15:05:59 -0300

    Group By con otras funciones

--- group_by_con_otras_funciones.sql
+++ group_by_con_otras_funciones.sql
@@ -1,3 +1,3 @@
 -- Extraer el dominio de los correos electrónicos
 
-SELECT first_name, last_name, SUBSTRING(email, POSITION('@' in email) + 1) AS Domain from users;
+SELECT first_name, last_name, SUBSTRING(email, POSITION('@' in email) + 1) AS Domain FROM users GROUP BY Domain HAVING COUNT(*) > 1;
