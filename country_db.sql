c03cae1f217a12254bccfe58c04d667cd77bbb72
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-05-10 07:04:23 -0300

    Creando índices

--- country_db.sql
+++ country_db.sql
@@ -27,3 +27,22 @@ SELECT * FROM country WHERE region = 'Central America';
 
 -- Actualizar el campo continent para los registros que tienen region = 'Central America'
 UPDATE country SET continent = 'Central America' WHERE region = 'Central America';
+
+SELECT * FROM country WHERE continent = 'South America';
+
+-- Creamos un índice único para el campo name
+-- Este índice es único, ya que no puede haber dos países con el mismo nombre
+-- En caso de que haya duplicados, se generará un error al intentar crear el índice
+-- Este índice se utiliza para mejorar el rendimiento de las consultas que filtran por nombre de país
+-- El índice se llama "unique_country_name" y se crea en la tabla country
+-- El índice se crea en el campo name
+-- El índice se crea como único, lo que significa que no se permiten valores duplicados en el campo name
+-- El índice se crea en la tabla country
+CREATE UNIQUE INDEX "unique_country_name" on country (name);
+
+-- Creamos un índice para el campo continent
+-- Este índice no es único, ya que puede haber varios países en el mismo continente
+-- En caso de que haya duplicados, no se generará un error al intentar crear el índice
+-- Este índice se utiliza para mejorar el rendimiento de las consultas que filtran por continente
+-- El índice se llama "country_continent" y se crea en la tabla country
+CREATE INDEX "country_continent" on country (continent);
