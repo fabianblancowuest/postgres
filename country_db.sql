ca3df1dcef887f6f0a8abca9de3dd6c847496d98
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-05-10 05:53:55 -0300

    Check con múltiples posibilidades strings

--- country_db.sql
+++ country_db.sql
@@ -10,3 +10,11 @@ ALTER TABLE country ADD PRIMARY KEY (code);
 
 -- Añadir CONSTRAINT CHECK para verificar que en el campo surfacearea se carguen datos con valores positivos
 ALTER TABLE country ADD CHECK(surfacearea >= 0);
+
+-- Añadir CONSTRAINT CHECK para verificar que en el campo continent se carguen datos con valores válidos
+ALTER TABLE
+    country
+ADD
+    CHECK(
+        continent IN('Europe', 'South America', 'North America', 'Oceania', 'Antarctica', 'Africa', 'Asia', 'Central America')
+    );
