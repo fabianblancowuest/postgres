c5e192acd545b8a297405838c6225d796f357258
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-05-15 15:16:11 -0300

    INNER JOIN

--- join_unions.sql
+++ join_unions.sql
@@ -4,3 +4,6 @@ SELECT * FROM continent WHERE code in (3,5) ORDER BY name;
 
 -- Consulta entre dos tablas usando where
 select a."name" as country, b."name" from country a, continent b where a.continent = b.code order by a."name";
+
+-- Consulta entre dos tablas usando inner join
+select a."name" as country, b."name" as continent from country a inner join continent b on a.continent = b.code order by a."name";
