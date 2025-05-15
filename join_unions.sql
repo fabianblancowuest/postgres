d0510161336b7c834f2b08467e14083473a10cc5
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-05-15 15:00:07 -0300

    Unión de tablas - Where

--- join_unions.sql
+++ join_unions.sql
@@ -1,3 +1,6 @@
 SELECT * FROM continent WHERE name LIKE '%America%' UNION
 
 SELECT * FROM continent WHERE code in (3,5) ORDER BY name;
+
+-- Consulta entre dos tablas usando where
+select a."name" as country, b."name" from country a, continent b where a.continent = b.code order by a."name";
